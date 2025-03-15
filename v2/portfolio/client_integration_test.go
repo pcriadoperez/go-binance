66ca338f11293198ea4f4363086aa4e909cea08e
Pablo <pablo@criadoperez.com>
2025-03-16 00:53:01 +0100

    fix client to get keys from os

--- v2/portfolio/client_integration_test.go
+++ v2/portfolio/client_integration_test.go
@@ -1,6 +1,7 @@
 package portfolio
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -13,8 +14,8 @@ type baseIntegrationTestSuite struct {
 
 func SetupTest(t *testing.T) *baseIntegrationTestSuite {
 	// TODO: get from env
-	apiKey := "z2aSOxQ9qPS0RIxS17ns2zN7UjMVoPF7jvTvpUJmnTlTZoj25GcnHdyPQXeQDJk9"
-	secretKey := "X1p9TkTnfbPTmYsbO3eJGI5I338XMmiFCF9AE3U5NxbS5dVZE6qZy8RqWXg1dcGF"
+	apiKey := os.Getenv("BINANCE_API_KEY")
+	secretKey := os.Getenv("BINANCE_SECRET_KEY")
 
 	if apiKey == "" || secretKey == "" {
 		t.Skip("API key and secret are required for integration tests")
